3847f393bb91b8f82354d6b3e3a5049fa2ef3bf0
Patchayanee Massakang <Patchayanee.m1224@gmail.com>
2020-08-15 16:02:03 +0700

    hello Github

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function user(){
+
+    }
+    public function show(){
+
+    }
+    public function create(){
+
+    }
+    public function store(){
+
+    }
+    public function  edit(){
+
+    }
+    public function update(){
+
+    }
+
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+    }
+    public function user(){
+
+    }
+    public function show(){
+
+    }
+    public function create(){
+
+    }
+    public function store(){
+
+    }
+    public function  edit(){
+
+    }
+    public function update(){
+
+    }
+    public function  delete(){
+
+    }
+}
